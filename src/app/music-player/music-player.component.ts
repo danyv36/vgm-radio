862b7a1616444029fdf201bbb7f102cbfaa3384a
Daniela <Daniela.Martinez2@Chubb.com>
2020-04-03 17:03:25 -0600

    trying to load the songs...

--- src/app/music-player/music-player.component.ts
+++ src/app/music-player/music-player.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SongService } from '../song.service';
+import { Subscription } from 'rxjs';
+import { ISong } from '../models/songs.model';
 
 @Component({
   selector: 'app-music-player',
   templateUrl: './music-player.component.html',
   styleUrls: ['./music-player.component.css']
 })
-export class MusicPlayerComponent implements OnInit {
+export class MusicPlayerComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+  songs: ISong[];
+
+  constructor(private songService: SongService) { }
 
   ngOnInit(): void {
+    this.subscription = this.songService.getAll().subscribe((songs: ISong[]) => {
+      console.log('music player songs fetched::', songs);
+      this.songs = songs;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
 
