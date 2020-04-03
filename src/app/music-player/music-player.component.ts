fb00e2ff967762aee925fa1dfa5b0484483fa93c
Daniela <Daniela.Martinez2@Chubb.com>
2020-04-03 16:38:43 -0600

    music player looks and loads better now...

--- src/app/music-player/music-player.component.ts
+++ src/app/music-player/music-player.component.ts
@@ -10,25 +10,6 @@ export class MusicPlayerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.loadScript();
   }
-
-  public loadScript() {
-    const body = <HTMLDivElement>document.body;
-
-    const script2 = document.createElement('script');
-    script2.innerHTML = '';
-    script2.src = '../assets/sound-manager/js/soundmanager-src/script/soundmanager2.js';
-    script2.async = true;
-    script2.defer = true;
-    body.appendChild(script2);
-
-    const script = document.createElement('script');
-    script.innerHTML = '';
-    script.src = '../assets/sound-manager/js/bar-ui.js';
-    script.async = true;
-    script.defer = true;
-    body.appendChild(script);
-  }
-
 }
+
